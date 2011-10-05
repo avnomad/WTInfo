7953ba473318cd508e491de461061644b8d8962d
Nomad <gd.nightmare@gmail.com>
2011-10-05 18:34:06 +0300

    Finished with contexts. Removed some 'φ's that resulted from copying and pasting from the web documentation to Visual Studio. Visual Studio didn't show the characters but inserted them in the strings, thus making them appear in the program output. added some more functions. minor tweaking of the column widths.

--- WTInfo/formating.h
+++ WTInfo/formating.h
@@ -79,6 +79,11 @@ wstring toBinary(UINT number)
 	return s;
 } // end function toBinary
 
+double toFloatingPoint(FIX32 fixPointNumber)
+{
+	return (fixPointNumber>>16)+double(0x0000ffff&fixPointNumber)/0x00010000;
+} // end function toFloatingPoint
+
 struct WTCOptions
 {
 	WTCOptions(UINT options,Indent indentation,UINT cw1,UINT cw2,UINT cw3)
@@ -93,11 +98,11 @@ struct WTCOptions
 
 std::wostream &operator<<(std::wostream &wout, const WTCOptions &options)
 {
-	wout << options.indent << left << setw(options.w1) << L"Flag" << right << setw(options.w2) << L"Value" << L"\n";
+	wout << options.indent << left << setw(options.w1) << L"Option" << right << setw(options.w2) << L"Value" << L"\n";
 	wout << options.indent << left << setw(options.w1) << L"CXO_SYSTEM" << right << setw(options.w2) << bool(CXO_SYSTEM&options.o)
 		<< Indent(options.w3) << L"Specifies that the context is a system cursor context." << L"\n";
 	wout << options.indent << left << setw(options.w1) << L"CXO_PEN" << right << setw(options.w2) << bool(CXO_PEN&options.o)
-		<< Indent(options.w3) << L"Specifies that the context is a Pen Windows context, if Pen Windows is in­stalled. The context is also a system cursor context; specifying CXO_PEN implies CXO_SYSTEM." << L"\n";
+		<< Indent(options.w3) << L"Specifies that the context is a Pen Windows context, if Pen Windows is installed. The context is also a system cursor context; specifying CXO_PEN implies CXO_SYSTEM." << L"\n";
 	wout << options.indent << left << setw(options.w1) << L"CXO_MESSAGES" << right << setw(options.w2) << bool(CXO_MESSAGES&options.o)
 		<< Indent(options.w3) << L"Specifies that the context returns WT_PACKET messages to its owner." << L"\n";
 	wout << options.indent << left << setw(options.w1) << L"CXO_MARGIN" << right << setw(options.w2) << bool(CXO_MARGIN&options.o)
@@ -120,13 +125,13 @@ struct WTDataMask
 	UINT w2;
 	UINT w3;
 	WORD iVersion;
-}; // end struct WTCOptions
+}; // end struct WTDataMask
 
 std::wostream &operator<<(std::wostream &wout, const WTDataMask &mask)
 {
 	wout << mask.indent << left << setw(mask.w1) << L"Flag" << right << setw(mask.w2) << L"Value" << L"\n";
 	wout << mask.indent << left << setw(mask.w1) << L"PK_CONTEXT" << right << setw(mask.w2) << bool(PK_CONTEXT&mask.m)
-		<< Indent(mask.w3) << L"Specifies the handle of the reporting con­text." << L"\n";
+		<< Indent(mask.w3) << L"Specifies the handle of the reporting context." << L"\n";
 	wout << mask.indent << left << setw(mask.w1) << L"PK_STATUS" << right << setw(mask.w2) << bool(PK_STATUS&mask.m)
 		<< Indent(mask.w3) << L"Specifies status information." << L"\n";
 	wout << mask.indent << left << setw(mask.w1) << L"PK_TIME" << right << setw(mask.w2) << bool(PK_TIME&mask.m)
@@ -148,13 +153,166 @@ std::wostream &operator<<(std::wostream &wout, const WTDataMask &mask)
 	wout << mask.indent << left << setw(mask.w1) << L"PK_NORMAL_PRESSURE" << right << setw(mask.w2) << bool(PK_NORMAL_PRESSURE&mask.m)
 		<< Indent(mask.w3) << L"Specify tip-button or normal-to-surface pressure data." << L"\n";
 	wout << mask.indent << left << setw(mask.w1) << L"PK_TANGENT_PRESSURE" << right << setw(mask.w2) << bool(PK_TANGENT_PRESSURE&mask.m)
-		<< Indent(mask.w3) << L"Specify barrel-button or tan­gent-to-surface pressure data." << L"\n";
+		<< Indent(mask.w3) << L"Specify barrel-button or tangent-to-surface pressure data." << L"\n";
 	wout << mask.indent << left << setw(mask.w1) << L"PK_ORIENTATION" << right << setw(mask.w2) << bool(PK_ORIENTATION&mask.m)
-		<< Indent(mask.w3) << L"Specifies cursor orientation informa­tion." << L"\n";
-	if(mask.iVersion >= 0x0101) 
+		<< Indent(mask.w3) << L"Specifies cursor orientation information." << L"\n";
+	if(mask.iVersion >= 0x0101)
 		wout << mask.indent << left << setw(mask.w1) << L"PK_ROTATION" << right << setw(mask.w2) << bool(PK_ROTATION&mask.m)
-			<< Indent(mask.w3) << L"Specifies cursor rotation informa­tion." << L"\n";
+			<< Indent(mask.w3) << L"Specifies cursor rotation information." << L"\n";
 	wout << L"\n";
 	return wout;
 } // end function operator<<
 
+struct WTCLocks
+{
+	WTCLocks(UINT locks,Indent indentation,UINT cw1,UINT cw2,UINT cw3)
+		:l(locks),indent(indentation),w1(cw1),w2(cw2),w3(cw3){}
+
+	UINT l;
+	Indent indent;
+	UINT w1;
+	UINT w2;
+	UINT w3;
+}; // end struct WTCLocks
+
+std::wostream &operator<<(std::wostream &wout, const WTCLocks &locks)
+{
+	wout << locks.indent << left << setw(locks.w1) << L"Lock" << right << setw(locks.w2) << L"Value" << L"\n";
+	wout << locks.indent << left << setw(locks.w1) << L"CXL_INSIZE" << right << setw(locks.w2) << bool(CXL_INSIZE&locks.l)
+		<< Indent(locks.w3) << L"Specifies that the context's input size cannot be changed. When this value is not specified, the context's input extents in x, y, and z can be changed. NOTE: The context's origins in x, y, and z can always be changed." << L"\n";
+	wout << locks.indent << left << setw(locks.w1) << L"CXL_INASPECT" << right << setw(locks.w2) << bool(CXL_INASPECT&locks.l)
+		<< Indent(locks.w3) << L"Specifies that the context's input aspect ratio cannot be changed. When this value is specified, the context's size can be changed, but the ratios among x, y, and z extents will be kept as close to constant as possible." << L"\n";
+	wout << locks.indent << left << setw(locks.w1) << L"CXL_MARGIN" << right << setw(locks.w2) << bool(CXL_MARGIN&locks.l)
+		<< Indent(locks.w3) << L"Specifies that the context's margin options cannot be changed. This value controls the locking of the CXO_MARGIN and CXO_MGNINSIDE option values." << L"\n";
+	wout << locks.indent << left << setw(locks.w1) << L"CXL_SENSITIVITY" << right << setw(locks.w2) << bool(CXL_SENSITIVITY&locks.l)
+		<< Indent(locks.w3) << L"Specifies that the context's sensitivity settings for x, y, and z cannot be changed." << L"\n";
+	wout << locks.indent << left << setw(locks.w1) << L"CXL_SYSOUT" << right << setw(locks.w2) << bool(CXL_SYSOUT&locks.l)
+		<< Indent(locks.w3) << L"If the context is a system cursor context, the value specifies that the sys­tem pointing control variables of the context cannot be changed." << L"\n";
+	wout << L"\n";
+	return wout;
+} // end function operator<<
+
+struct WTCStatus
+{
+	WTCStatus(UINT status,Indent indentation,UINT cw1,UINT cw2,UINT cw3)
+		:s(status),indent(indentation),w1(cw1),w2(cw2),w3(cw3){}
+
+	UINT s;
+	Indent indent;
+	UINT w1;
+	UINT w2;
+	UINT w3;
+}; // end struct WTCStatus
+
+std::wostream &operator<<(std::wostream &wout, const WTCStatus &status)
+{
+	wout << status.indent << left << setw(status.w1) << L"Flag" << right << setw(status.w2) << L"Value" << L"\n";
+	wout << status.indent << left << setw(status.w1) << L"CXS_DISABLED" << right << setw(status.w2) << bool(CXS_DISABLED&status.s)
+		<< Indent(status.w3) << L"Specifies that the context has been disabled using the WTEnable function." << L"\n";
+	wout << status.indent << left << setw(status.w1) << L"CXS_OBSCURED" << right << setw(status.w2) << bool(CXS_OBSCURED&status.s)
+		<< Indent(status.w3) << L"Specifies that the context is at least partially obscured by an overlapping con­text that is higher in the context overlap order." << L"\n";
+	wout << status.indent << left << setw(status.w1) << L"CXS_ONTOP" << right << setw(status.w2) << bool(CXS_ONTOP&status.s)
+		<< Indent(status.w3) << L"Specifies that the context is the topmost context in the context overlap order." << L"\n";
+	wout << L"\n";
+	return wout;
+} // end function operator<<
+
+struct ExtLogContext 
+{
+	ExtLogContext(Indent indentation,UINT cw1,UINT cw2,UINT cw3,WORD version,Indent subIndent,UINT scw1,UINT scw2,UINT scw3)
+		:indent(indentation),w1(cw1),w2(cw2),w3(cw3),iVersion(version),sIndent(subIndent),sw1(scw1),sw2(scw2),sw3(scw3){}
+
+	LOGCONTEXTW c;
+	Indent indent;
+	Indent sIndent;
+	UINT w1;
+	UINT w2;
+	UINT w3;
+	UINT sw1;
+	UINT sw2;
+	UINT sw3;
+	WORD iVersion;
+}; // end struct ExtLogContext
+
+std::wostream &operator<<(std::wostream &wout, const ExtLogContext &context)
+{
+	std::wostringstream wsout;
+	wout << context.indent << left << setw(context.w1) << L"Flag" << right << setw(context.w2) << L"Value" << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_NAME" << right << setw(context.w2) << context.c.lcName
+		<< Indent(context.w3) << L"Returns a 40 character array containing the default name. The name may occupy zero to 39 characters; the remainder of the array is padded with zeroes." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OPTIONS" << right << setw(context.w2) << toBinary(context.c.lcOptions)
+		<< Indent(context.w3) << L"Returns option flags. For the default digitizing context, CXO_MARGIN and CXO_MGNINSIDE are allowed. For the default system context, CXO_SYSTEM is required; CXO_PEN, CXO_MARGIN, and CXO_MGNINSIDE are allowed." << L"\n";
+	wout << WTCOptions(context.c.lcOptions,context.sIndent,context.sw1,context.sw2,context.sw3);
+	wout << context.indent << left << setw(context.w1) << L"CTX_STATUS" << right << setw(context.w2) << toBinary(context.c.lcStatus)
+		<< Indent(context.w3) << L"Returns zero." << L"\n";
+	wout << WTCStatus(context.c.lcStatus,context.sIndent,context.sw1,context.sw2,context.sw3);
+	wout << context.indent << left << setw(context.w1) << L"CTX_LOCKS" << right << setw(context.w2) << toBinary(context.c.lcLocks)
+		<< Indent(context.w3) << L"Returns which attributes of the default context are locked." << L"\n";
+	wout << WTCLocks(context.c.lcLocks,context.sIndent,context.sw1,context.sw2,context.sw3);
+	wsout << L"0x" << std::hex << std::uppercase << std::setfill(L'0') << setw(2*sizeof(UINT)) << context.c.lcMsgBase;
+	wout << context.indent << left << setw(context.w1) << L"CTX_MSGBASE" << right << setw(context.w2) << wsout.str()
+		<< Indent(context.w3) << L"Returns the value WT_DEFBASE." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_DEVICE" << right << setw(context.w2) << (INT)context.c.lcDevice
+		<< Indent(context.w3) << L"Returns the default device.  If this value is -1, then it also known as a \"virtual device\"." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_PKTRATE" << right << setw(context.w2) << context.c.lcPktRate
+		<< Indent(context.w3) << L"Returns the default context packet report rate, in Hertz." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_PKTDATA" << right << setw(context.w2) << toBinary(context.c.lcPktData)
+		<< Indent(context.w3) << L"Returns which optional data items will be in packets returned from the context. For the default digitizing context, this field must at least indicate buttons, x, and y data." << L"\n";
+	wout << WTDataMask(context.c.lcPktData,context.sIndent,context.sw1,context.sw2,context.sw3,context.iVersion);
+	wout << context.indent << left << setw(context.w1) << L"CTX_PKTMODE" << right << setw(context.w2) << toBinary(context.c.lcPktMode)
+		<< Indent(context.w3) << L"Returns whether the packet data items will be returned in absolute or relative mode." << L"\n";
+	wout << WTDataMask(context.c.lcPktMode,context.sIndent,context.sw1,context.sw2,context.sw3,context.iVersion);
+	wout << context.indent << left << setw(context.w1) << L"CTX_MOVEMASK" << right << setw(context.w2) << toBinary(context.c.lcMoveMask)
+		<< Indent(context.w3) << L"Returns which packet data items can generate motion events in the context." << L"\n";
+	wout << WTDataMask(context.c.lcMoveMask,context.sIndent,context.sw1,context.sw2,context.sw3,context.iVersion);
+	wout << context.indent << left << setw(context.w1) << L"CTX_BTNDNMASK" << right << setw(context.w2) << toBinary(context.c.lcBtnDnMask)
+		<< Indent(context.w3) << L"Returns the buttons for which button press events will be processed in the context. The default context must at least select button press events for one button." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_BTNUPMASK" << right << setw(context.w2) << toBinary(context.c.lcBtnUpMask)
+		<< Indent(context.w3) << L"Returns the buttons for which button release events will be processed in the context." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INORGX" << right << setw(context.w2) << context.c.lcInOrgX
+		<< Indent(context.w3) << L"Returns the origin of the context's input area in the tablet's native coordinates, along the x axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INORGY" << right << setw(context.w2) << context.c.lcInOrgY
+		<< Indent(context.w3) << L"Returns the origin of the context's input area in the tablet's native coordinates, along the y axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INORGZ" << right << setw(context.w2) << context.c.lcInOrgZ
+		<< Indent(context.w3) << L"Returns the origin of the context's input area in the tablet's native coordinates, along the z axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INEXTX" << right << setw(context.w2) << context.c.lcInExtX
+		<< Indent(context.w3) << L"Returns the extent of the context's input area in the tablet's native coordinates, along the x axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INEXTY" << right << setw(context.w2) << context.c.lcInExtY
+		<< Indent(context.w3) << L"Returns the extent of the context's input area in the tablet's native coordinates, along the y axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_INEXTZ" << right << setw(context.w2) << context.c.lcInExtZ
+		<< Indent(context.w3) << L"Returns the extent of the context's input area in the tablet's native coordinates, along the z axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTORGX" << right << setw(context.w2) << context.c.lcOutOrgX
+		<< Indent(context.w3) << L"Returns the origin of the context's output coordinate space in context output coordinates, along the x axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTORGY" << right << setw(context.w2) << context.c.lcOutOrgY
+		<< Indent(context.w3) << L"Returns the origin of the context's output coordinate space in context output coordinates, along the y axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTORGZ" << right << setw(context.w2) << context.c.lcOutOrgZ
+		<< Indent(context.w3) << L"Returns the origin of the context's output coordinate space in context output coordinates, along the z axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTEXTX" << right << setw(context.w2) << context.c.lcOutExtX
+		<< Indent(context.w3) << L"Returns the extent of the context's output coordinate space in context output coordinates, along the x axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTEXTY" << right << setw(context.w2) << context.c.lcOutExtY
+		<< Indent(context.w3) << L"Returns the extent of the context's output coordinate space in context output coordinates, along the y axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_OUTEXTZ" << right << setw(context.w2) << context.c.lcOutExtZ
+		<< Indent(context.w3) << L"Returns the extent of the context's output coordinate space in context output coordinates, along the z axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SENSX" << right << setw(context.w2) << toFloatingPoint(context.c.lcSensX)
+		<< Indent(context.w3) << L"Returns the relative-mode sensitivity factor, along the x axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SENSY" << right << setw(context.w2) << toFloatingPoint(context.c.lcSensY)
+		<< Indent(context.w3) << L"Returns the relative-mode sensitivity factor, along the y axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SENSZ" << right << setw(context.w2) << toFloatingPoint(context.c.lcSensZ)
+		<< Indent(context.w3) << L"Returns the relative-mode sensitivity factor, along the z axis." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSMODE" << right << setw(context.w2) << context.c.lcSysMode
+		<< Indent(context.w3) << L"Returns the default system cursor tracking mode." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSORGX" << right << setw(context.w2) << context.c.lcSysOrgX
+		<< Indent(context.w3) << L"Returns 0." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSORGY" << right << setw(context.w2) << context.c.lcSysOrgY
+		<< Indent(context.w3) << L"Returns 0." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSEXTX" << right << setw(context.w2) << context.c.lcSysExtX
+		<< Indent(context.w3) << L"Returns the current screen display size in pixels, in the x direction." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSEXTY" << right << setw(context.w2) << context.c.lcSysExtY
+		<< Indent(context.w3) << L"Returns the current screen display size in pixels, in the y direction." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSSENSX" << right << setw(context.w2) << toFloatingPoint(context.c.lcSysSensX)
+		<< Indent(context.w3) << L"Returns the system cursor relative-mode sensitivity factor, in the x direction." << L"\n";
+	wout << context.indent << left << setw(context.w1) << L"CTX_SYSSENSY" << right << setw(context.w2) << toFloatingPoint(context.c.lcSysSensY)
+		<< Indent(context.w3) << L"Returns the system cursor relative-mode sensitivity factor, in the y direction." << L"\n";
+	wout << L"\n";
+	return wout;
+} // end function operator<<
--- WTInfo/main.cpp
+++ WTInfo/main.cpp
@@ -137,7 +137,8 @@ int main()
 
 	// actual information
 	UINT c1 = 65;
-	cw1=15,cw2=40,cw3=6;
+	cw1=18,cw2=37,cw3=6;
+	UINT sIndent=27,scw1=25,scw2=7,scw3=6;
 	wcout << std::boolalpha;
 	wcout << indent << left << setw(c1) << L"Category/Index" << L"Description" << L"\n\n";
 	wcout << indent << left << setw(c1) << L"WTI_INTERFACE" << L"Contains global interface identification and capability information." << L"\n";
@@ -146,7 +147,7 @@ int main()
 	block_size = WTInfoW(WTI_INTERFACE,IFC_WINTABID,nullptr);
 	block = unique_ptr<char[]>(new char[block_size]);
 	WTInfoW(WTI_INTERFACE,IFC_WINTABID,block.get());
-	wcout << indent << left << setw(cw1) << L"IFC_WINTABID" << right << setw(cw2) << (wchar_t*)block.get() << Indent(cw3) << L"Returns a copy of the null-terminated tablet hardware identifica­tion string in the user buffer. This string should include make, model, and revi­sion information in user-readable format." << L"\n";
+	wcout << indent << left << setw(cw1) << L"IFC_WINTABID" << right << setw(cw2) << (wchar_t*)block.get() << Indent(cw3) << L"Returns a copy of the null-terminated tablet hardware identification string in the user buffer. This string should include make, model, and revision information in user-readable format." << L"\n";
 	wcout << indent << left << setw(cw1) << L"IFC_SPECVERSION" << right << setw(cw2) << WTVersion(specVersion) << Indent(cw3) << L"Returns the specification version number. The high-order byte contains the major version number; the low-order byte contains the minor version number." << L"\n";
 	wcout << indent << left << setw(cw1) << L"IFC_IMPLVERSION" << right << setw(cw2) << WTVersion(implVersion) << Indent(cw3) << L"Returns the implementation version number. The high-order byte contains the major version number; the low-order byte contains the minor version number." << L"\n";
 	wcout << indent << left << setw(cw1) << L"IFC_NDEVICES" << right << setw(cw2) << nDevices << Indent(cw3) << L"Returns the number of devices supported." << L"\n";
@@ -154,7 +155,7 @@ int main()
 	wcout << indent << left << setw(cw1) << L"IFC_NCONTEXTS" << right << setw(cw2) << nContexts << Indent(cw3) << L"Returns the number of contexts supported." << L"\n";
 	WTInfoW(WTI_INTERFACE,IFC_CTXOPTIONS,&options);
 	wcout << indent << left << setw(cw1) << L"IFC_CTXOPTIONS" << right << setw(cw2) << toBinary(options) << Indent(cw3) << L"Returns flags indicating which context options are supported." << L"\n";
-	wcout << WTCOptions(options,27,25,7,6);
+	wcout << WTCOptions(options,sIndent,scw1,scw2,scw3);
 	wcout << indent << left << setw(cw1) << L"IFC_CTXSAVESIZE" << right << setw(cw2) << save_size << Indent(cw3) << L"Returns the size of the save information returned from WTSave." << L"\n";
 	wcout << indent << left << setw(cw1) << L"IFC_NEXTENSIONS" << right << setw(cw2) << nExtensions << Indent(cw3) << L"Returns the number of extension data items supported." << L"\n";
 	wcout << indent << left << setw(cw1) << L"IFC_NMANAGERS" << right << setw(cw2) << nManagers << Indent(cw3) << L"Returns the number of manager handles supported." << L"\n";
@@ -167,19 +168,47 @@ int main()
 	wcout << indent << left << setw(cw1) << L"STA_PKTRATE" << right << setw(cw2) << curPktRate << Indent(cw3) << L"Returns the maximum packet report rate currently being re­ceived by any context, in Hertz." << L"\n";
 	WTInfoW(WTI_STATUS,STA_PKTDATA,&dataMask);
 	wcout << indent << left << setw(cw1) << L"STA_PKTDATA" << right << setw(cw2) << toBinary(dataMask) << Indent(cw3) << L"Returns a mask indicating which packet data items are re­quested by at least one context." << L"\n";
-	wcout << WTDataMask(dataMask,27,25,7,6,specVersion);
+	wcout << WTDataMask(dataMask,sIndent,scw1,scw2,scw3,specVersion);
 	wcout << indent << left << setw(cw1) << L"STA_MANAGERS" << right << setw(cw2) << curManagers << Indent(cw3) << L"Returns the number of manager handles currently open." << L"\n";
 	wcout << indent << left << setw(cw1) << L"STA_SYSTEM" << right << setw(cw2) << globSysPointing << Indent(cw3) << L"Returns a non-zero value if system pointing is available to the whole screen; zero otherwise." << L"\n";
 	wcout << indent << left << setw(cw1) << L"STA_BUTTONUSE" << right << setw(cw2) << toBinary(buttonUse) << Indent(cw3) << L"Returns a button mask indicating the logical buttons whose events are requested by at least one context." << L"\n";
 	wcout << indent << left << setw(cw1) << L"STA_SYSBTNUSE" << right << setw(cw2) << toBinary(sysBtnUse) << Indent(cw3) << L"Returns a button mask indicating which logical buttons are as­signed a system button function by the current cursor's system button map." << L"\n";
 	--indent;
 	wcout << indent << left << setw(c1) << L"WTI_DEFCONTEXT" << L"Contains the current default digitizing logical context." << L"\n";
+	++indent;
+	ExtLogContext context(indent,cw1,cw2,cw3,specVersion,sIndent,scw1,scw2,scw3);
+	WTInfoW(WTI_DEFCONTEXT,0,&context);
+	wcout << context;
+	--indent;
 	wcout << indent << left << setw(c1) << L"WTI_DEFSYSCTX" << L"Contains the current default system logical context." << L"\n";
+	++indent;
+	WTInfoW(WTI_DEFSYSCTX,0,&context);
+	wcout << context;
+	--indent;
 	wcout << indent << left << setw(c1) << L"WTI_DEVICES" << L"Each contains capability and status information for a device." << L"\n";
 	wcout << indent << left << setw(c1) << L"WTI_CURSORS" << L"Each contains capability and status information for a cursor type." << L"\n";
 	wcout << indent << left << setw(c1) << L"WTI_EXTENSIONS" << L"Each contains descriptive information and defaults for an extension." << L"\n";
-	wcout << indent << left << setw(c1) << L"WTI_DDCTXS" << L"Each contains the current default digitizing logical context for the corre­sponding device." << L"\n";
-	wcout << indent << left << setw(c1) << L"WTI_DSCTXS" << L"Each contains the current default system logical context for the correspond­ing device." << L"\n";
+	wcout << indent << left << setw(c1) << L"WTI_DDCTXS" << L"Each contains the current default digitizing logical context for the corresponding device." << L"\n";
+	++indent;
+	++context.indent;
+	context.w1 -= 4;
+	cw1=9,cw2=3,cw3=2;
+	for(UINT c = 0 ; c < nDevices ; ++c)
+	{
+		wcout << indent << left << setw(cw1) << L"device" << right << setw(cw2) << c << L"\n";
+		WTInfoW(WTI_DDCTXS+c,0,&context);
+		wcout << context;
+	} // end for
+	--indent;
+	wcout << indent << left << setw(c1) << L"WTI_DSCTXS" << L"Each contains the current default system logical context for the corresponding device." << L"\n";
+	++indent;
+	for(UINT c = 0 ; c < nDevices ; ++c)
+	{
+		wcout << indent << left << setw(cw1) << L"device" << right << setw(cw2) << c << L"\n";
+		WTInfoW(WTI_DSCTXS+c,0,&context);
+		wcout << context;
+	} // end for
+	--indent;
 
 	wcout << L"\n\n\n";
 
